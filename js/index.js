416eeca184691cbc28a55c5ecaeee4d9f8c6ac8e
laggart <laggart@gmail.com>
2022-08-27 17:34:50 +0200

    iteracion 1 y 2 done, la 3 me la tengo que repensar

--- js/index.js
+++ js/index.js
@@ -1,23 +1,30 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
+  const price = product.querySelector('.price span');
+  const  quantity = product.querySelector('.quantity input')
+
+  const subtotalValue = Number(price.innerHTML) * Number(quantity.value);
+
+  product.querySelector('.subtotal span').innerHTML = subtotalValue
+ 
+ 
   //... your code goes here
 }
 
-function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+// ITERATION 2
 
-  // ITERATION 2
-  //... your code goes here
+function calculateAll(event) {
+  const prices = document.getElementsByClassName('product')
+  const pricesArray = [...prices]
+  pricesArray.forEach(updateSubtotal)
+
+  /* console.log(pricesArray) */
+   
 
   // ITERATION 3
-  //... your code goes here
+
 }
 
 // ITERATION 4
