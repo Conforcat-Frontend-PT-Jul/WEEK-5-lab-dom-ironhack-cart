461239d7225b8148e91943ecc367a08ce59acebd
Gabi <gabi@mac-morales.local>
2022-09-02 00:32:56 +0200

    WEEK-5-lab-cart

--- js/index.js
+++ js/index.js
@@ -1,23 +1,45 @@
 // ITERATION 1
+//variable global
+let total = 0;
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+
+  console.log('Calculating subtotal, yey!');
+  const price = product.querySelector('.price span').innerHTML;
+  const quantity = product.querySelector('.quantity input').value;
+
+  let subTotalPrice = price * quantity;
+
+  total = total + subTotalPrice;
+
+  const subTotal = product.querySelector('.subtotal span');
+  //asigno el valor total al tag span 
+  subTotal.textContent = subTotalPrice;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
+
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+
+  const products = document.getElementsByClassName('product');
+
+  for (let i = 0; i < products.length; i++) {
+    updateSubtotal(products[i]);
+  }
 
   // ITERATION 3
-  //... your code goes here
+  const totalValue = document.querySelector('#total-value span');
+
+  totalValue.textContent = total;
+
 }
 
 // ITERATION 4
