0ec0f5dde517074f174e704cc31d8645e073859a
Gabi <gabi@mac-morales.local>
2022-09-02 22:43:23 +0200

    WEEK-5-lab-cart

--- js/index.js
+++ js/index.js
@@ -19,6 +19,7 @@ function updateSubtotal(product) {
 }
 
 function calculateAll() {
+  total = 0
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
 
@@ -46,19 +47,47 @@ function calculateAll() {
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
+  // console.log('The target in remove is:', target);
   //... your code goes here
+
+const productFila = target.parentNode.parentNode;
+productFila.parentNode.removeChild(productFila);
+
+//Obtenemos el nodo subtotal del elemento borrado
+const subtTotal = productFila.querySelector('.subtotal span').innerHTML;
+//Recalculamos el valor total, restando el valor del elemento borrado
+total = total - subtTotal;
+// Obtenermos el elemento del valor total y actualizamos el valor
+const totalValue = document.querySelector('#total-value span');
+totalValue.textContent = total;
+  
 }
 
 // ITERATION 5
 
 function createProduct() {
+
   //... your code goes here
+
+  const addProductFila = document.getElementsByClassName('create-product')
+  // parent.appendChild(h2Tag);
+  // document.createElement("h1");
 }
 
+//Se ejecuta al cargar la página web
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  //Recuperar todos los botones "Remove" en el DOM
+  const removeButtons = document.getElementsByClassName('btn btn-remove');
+ 
+  //Asignar el evento click a cada botón "Remove"
+  for (let i = 0; i < removeButtons.length; i++) {
+    removeButtons[i].addEventListener('click', removeProduct);
+  }
+
+  const addProductBtn = document.getElementById('create');
+  addProductBtn.addEventListener('click', createProduct);
+
 });
